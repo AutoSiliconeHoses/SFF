7ce9f695838382a0e4a5d49385761a4530702ba5
Daniel Wise <daniel@autosiliconehoses.com>
2018-04-27 16:31:39 +0100

    Replaced fnr.exe in ps1 Demo

--- StockFeed/ToolBankFeed/toolbank.ps1
+++ StockFeed/ToolBankFeed/toolbank.ps1
@@ -1,4 +1,5 @@
 $Host.UI.RawUI.WindowTitle = "ToolBankFeed"
+Set-PSDebug -Trace 1
 Z:
 cd "Z:\Stock File Fetcher\StockFeed\ToolBankFeed\Scripts"
 ftp -s:login.txt 195.74.141.134
@@ -8,20 +9,31 @@ Rename-Item Availability20D.csv stock.csv
 del stock.csv
 
 cd "Z:\Stock File Fetcher\Upload"
+
 If (Test-Path -Path "toolbank.txt"){
   del toolbank.txt
 }
 
-& "Z:\Stock File Fetcher\StockFeed\ToolBankFeed\Scripts\OpenAndSave2.vbs" /C
-& "Z:\Stock File Fetcher\StockFeed\ToolBankFeed\Scripts\OpenAndSave.vbs" /C
-& "Z:\Stock File Fetcher\StockFeed\ToolBankFeed\Scripts\SaveAsTxt.vbs" /C
-
 cd "Z:\Stock File Fetcher\StockFeed\ToolBankFeed"
+
+"OpenAndSave2.vbs"
+& "Z:\Stock File Fetcher\StockFeed\ToolBankFeed\Scripts\OpenAndSave2.vbs" /C
+"OpenAndSave.vbs"
+& "Z:\Stock File Fetcher\StockFeed\ToolBankFeed\Scripts\OpenAndSave.vbs" /C
+"SaveAsTxt.vbs"
+& "Z:\Stock File Fetcher\StockFeed\ToolBankFeed\Scripts\SaveAsTxt.vbs" /C
+ls
+"Replacing String"
+(Get-Content 'Z:\Stock File Fetcher\StockFeed\ToolBankFeed\toolbank.txt').replace("FALSE`t`t`t`t0`t5", "")   | Set-Content 'Z:\Stock File Fetcher\StockFeed\ToolBankFeed\toolbank.txt'
+
 If (Test-Path -Path "toolbankgrep.txt"){
   del toolbankgrep.txt
 }
 
 findstr "[[A-Z] [0-9] ,]" toolbank.txt > toolbankgrep.txt
+If (Test-Path -Path "toolbank.txt"){
+  del toolbank.txt
+}
 Rename-Item toolbankgrep.txt toolbank.txt
 
 move toolbank.txt "Z:\Stock File Fetcher\Upload"
