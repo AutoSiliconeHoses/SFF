c93c2be31e2c8ae33a2a6c0f16ba050cba20bc85
Jack Powell <jackp@autosiliconehoses.com>
2018-05-08 17:07:26 +0100

    Updated fnr commands

--- StockFeed/ToolBankFeed/toolbank.bat
+++ StockFeed/ToolBankFeed/toolbank.bat
@@ -7,6 +7,7 @@ ren Availability20D.csv stock.csv
 %comspec% /C "Z:\Stock File Fetcher\StockFeed\ToolBankFeed\Scripts\SaveAsTxt.vbs"
 
 "Z:\Stock File Fetcher\StockFeed\Programs\fnr.exe" --cl --dir "Z:\Stock File Fetcher\StockFeed\ToolBankFeed" --fileMask "*toolbank.txt*" --excludeFileMask "*.dll, *.exe" --caseSensitive --find "FALSE				0	4" --replace ""
+"Z:\Stock File Fetcher\StockFeed\Programs\fnr.exe" --cl --dir "Z:\Stock File Fetcher\StockFeed\ToolBankFeed" --fileMask "*toolbank.txt*" --excludeFileMask "*.dll, *.exe" --caseSensitive --find "#REF!				#REF!	4" --replace ""
 
 cd "Z:\Stock File Fetcher\StockFeed\ToolBankFeed"
 findstr "[[A-Z] [0-9] ,]" toolbank.txt > toolbankgrep.txt
