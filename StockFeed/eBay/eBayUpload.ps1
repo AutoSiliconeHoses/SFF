502c6c6d6271da129ffd97ae9a92252b2c39f12a
Daniel Wise <daniel@autosiliconehoses.com>
2019-04-20 09:49:58 +0100

    Added alterations
    Now uploads ash-FULLSTOCK files after altering stock from the alterations.csv list
    This makes keeping skus at 0 a breeze.

--- StockFeed/eBay/eBayUpload.ps1
+++ StockFeed/eBay/eBayUpload.ps1
@@ -21,6 +21,7 @@ $functions = {
       $script:MiddlePoint = -($LowIndex + 1)
       return
   }
+
   function CreateStockfile ($store, $supplier) {
       #Load Files into RAM
       $FileA = Import-CSV ("\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\StoreFiles\$store\"+$supplier+"-FULLSTOCK.csv")
@@ -64,19 +65,46 @@ $functions = {
     #Add Data to File
     $FileC | Add-Content $FileCPath
   }
-}
 
-if ($args[0] -match "zero"){
-  $zero = $true
-  $keepPath = "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Stock\Keep"
-  $zeroPath = "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Stock\Zeroing"
+  function alter ($store) {
+    #Load Files into RAM
+    $FileA = Import-CSV ("\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\StoreFiles\" + $store + "\ash-FULLSTOCK.csv")
+    $FileB = Import-CSV "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\GUI\alterations.csv"
 
-  mkdir $keepPath
-  gci "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Stock" -filter "*.txt" |
-    % {mv $_.fullname $keepPath}
-  gci $zeroPath |
-    % {mv $_.fullname "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Stock"}
-  del $zeroPath
+    #Setup Output file location
+    $FileCPath = "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Updated\" + $store + "-ash.csv"
+    If (Test-Path $FileCPath) {del $FileCPath}
+    "Action(SiteID=UK|Country=GB|Currency=GBP|Version=585|CC=UTF-8),ItemID,SiteID,Quantity,Relationship,RelationshipDetails,CustomLabel" | Add-Content $FileCPath
+
+    $count = 0
+    #Build File Data
+    ForEach($lineA in $FileA) {
+      $match = $FALSE
+      Foreach ($lineB in $FileB) {
+        #Clean Sku ready for search
+        $lineAClean = $lineA.CustomLabel -replace "[\|#].*",""
+
+        #Convert TEX to TL
+        If ($supplier -eq "tetrosyl"){
+          $lineAClean = $lineAClean -replace "-TEX","-TL"
+        }
+
+        If ($lineAClean -match $lineB.sku) {
+          $lineC = "{0},{1},{2},{3},{4},{5},{6}`n" -f $lineA.'Action(SiteID=UK|Country=GB|Currency=GBP|Version=585|CC=UTF-8)',$lineA.ItemID,$lineA.SiteID,$lineB.qty,$lineA.Relationship,$lineA.RelationshipDetails,$lineA.CustomLabel
+          $match = $TRUE
+          $count++
+          break
+        }
+      }
+      If (!$match) {
+        $lineC = "{0},{1},{2},{3},{4},{5},{6}`n" -f $lineA.'Action(SiteID=UK|Country=GB|Currency=GBP|Version=585|CC=UTF-8)',$lineA.ItemID,$lineA.SiteID,$lineA.Quantity,$lineA.Relationship,$lineA.RelationshipDetails,$lineA.CustomLabel
+      }
+      $FileC += $lineC
+    }
+    #Add Data to File
+    Write-Host "$count alterations made"
+    $FileC | Add-Content $FileCPath
+  }
 }
 
 # The most horrid way of doing FPS known to man
@@ -123,18 +151,31 @@ Foreach ($store in $StoreList) {
       } -ArgumentList $store,$amazonFile.basename | Out-Null
     }
   }
+  $replenish = "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\StoreFiles\"+$store.'Store Code'+"\ash-FULLSTOCK.csv"
+  If (Test-Path $replenish) {
+    "`tCreating ASH file"
+    Start-Job -InitializationScript $functions -name ($store.'Store Code'+"-ash") -ScriptBlock {
+      #TODO: Create new file with alterations
+      $store = $args[0]
+      alter $store.'Store Code'
+
+      # Set up arguments for curl
+      $result = '"\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\UploadResults\'+ $store.'Store Code' + '-ash-RESULT.html"'
+      $token = '"token=' + $store.Token + '"'
+      #TODO: Change this path
+      $file = '"file=@' + "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Updated\" + $store.'Store Code' + '-ash.csv"'
+
+      # Concatonate command with arguments and invoke
+      $command = "curl.exe -k -o " + $result + " -F " + $token + " -F " + $file + " https://bulksell.ebay.com/ws/eBayISAPI.dll?FileExchangeUpload"
+      iex $command
+    } -ArgumentList $store | Out-Null
+  }
 }
 
 "Waiting for completion..."
-Wait-Job * -timeout 14400 | Out-Null
+Wait-Job * -timeout 43200 | Out-Null
 
 "Deleting used Stock Files"
-del "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Stock\*" -ErrorAction SilentlyContinue
-
-If ($zero) {
-  gci $keepPath |
-    % {mv $_.fullname "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Stock"}
-  del $keepPath
-}
+# del "\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\eBay\Stock\*.txt" -ErrorAction SilentlyContinue
 
 "Done"
